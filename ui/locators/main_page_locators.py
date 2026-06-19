6ed030177f48f2dfddaf1a8df504e2fb6bb03f06
Ruslan Kutorgin <teenxsky@gmail.com>
2026-06-19 13:36:38 +1000

    Add main page locators

---
+++ ui/locators/main_page_locators.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    # data-атрибуты
+    HEADER = (By.CSS_SELECTOR, "[data-ftid='component_header']")
+    SEARCH_VIN = (By.CSS_SELECTOR, "[data-ftid='autostory-widget_input']")
+
+    # пункты верхнего меню
+    MENU_CARS = (By.XPATH, "//a[@data-ftid='component_header_main-menu-item'][contains(.,'Автомобили')]")
+    MENU_REVIEWS = (By.XPATH, "//a[@data-ftid='component_header_main-menu-item'][contains(.,'Отзывы')]")
+    MENU_CATALOG = (By.XPATH, "//a[@data-ftid='component_header_main-menu-item'][contains(.,'Каталог')]")
